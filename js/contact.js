f04c9a08cf8000acdf08ba93491008e72d0613cd
cmdtrwd <jittrapol.i@psu.ac.th>
2026-02-25 11:18:14 +0700

    Clicking anywhere to close the menu

--- js/contact.js
+++ js/contact.js
@@ -2,7 +2,15 @@ document.addEventListener("DOMContentLoaded", function() {
     const bars = document.querySelector(".bars");
     const navbar = document.querySelector(".nav-bar");
     
-    bars.addEventListener("click", function() {
+    bars.addEventListener("click", function(event) {
         navbar.classList.toggle("active");
+        event.stopPropagation();
+    });
+
+    document.addEventListener("click", function(event) {
+        // Check if the menu is open and if the click was NOT inside the navbar
+        if (navbar.classList.contains("active") && !navbar.contains(event.target)) {
+            navbar.classList.remove("active");
+        }
     });
 });
